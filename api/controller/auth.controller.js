7080b5bdb952fc80899d4f2fc18be0ba4c771830
Usman-Choudharys <mrusmanghani8@gmail.com>
2024-03-11 07:15:42 +0500

    signin is completed

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,24 +1,57 @@
 import User from "../models/user.models.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req,res,next)=>{
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return next(errorHandler(400, "All fields are required"));
+  }
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashPassword,
+  });
 
-    const {username,email,password} = req.body;
-    if (!username || !email || !password || username==="" || email==="" || password==="") {
-        return next(errorHandler(400,"All fields are required"))
-    }
-     const hashPassword = bcrypt.hashSync(password,10);
-     const newUser = new User({
-        username,
-        email,
-        password:hashPassword
-     })
+  try {
+    await newUser.save();
+    res.status(200).json({ msg: "user has been created successfully" });
+  } catch (error) {
+    next(
+      errorHandler(400, "this user is already exist in the database stacks")
+    );
+  }
+};
 
-    try {
-        await newUser.save()
-        res.status(200).json({msg:"user has been created successfully"})
-    } catch (error) {
-      next(errorHandler(400,"this user is already exist in the database stacks"))
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email == "" || password == "") {
+   return  next(errorHandler(400, "Email and password are required"));
+  }
+  try {
+    const validateUser = await User.findOne({ email });
+    if (!validateUser) {
+     return next(errorHandler(404, "There is no register account plz signup "));
     }
-  
-}
+    const validatePassword = bcrypt.compareSync(
+      password,
+      validateUser.password
+    );
+    if (!validatePassword) {
+     return next(errorHandler(404, "the password is incorrect"));
+    }
+    const token = jwt.sign({ id: validateUser._id }, process.env.JWT_SECRET);
+    const{password:pass,...rest}= validateUser._doc;
+   return res.status(200).cookie("access_token",token,{httpOnly :true}).json(rest)
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
-function SignIn() {
+function SignUp() {
+  const [formData ,setformData] = useState({});
+  const [errorMessage,seterrorMessage] = useState(null);
+  const [loading ,setLoading] = useState(false);
+
+  const navigate = useNavigate()
+  const handleChange = (e)=>{
+    setformData({...formData,[e.target.id]:e.target.value.trim()})
+  }
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      seterrorMessage("All fields are required")
+    }
+    try {
+      setLoading(true);
+      seterrorMessage(null)
+      const res = await fetch("/api/signin",{
+        method: 'POST',
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(formData)
+      })
+      const data = await res.json()
+      if (data.success== false) {
+        seterrorMessage(data.message)
+      }
+  setLoading(false)
+  if (res.ok) {
+    navigate("/")
+  }
+    } catch (error) {
+      seterrorMessage(error.message)
+      setLoading(false)
+    }
+
+  }
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 mx-auto max-w-3xl flex-col md:flex-row gap-4">
+      {/* for left side */}
+      <div className="flex-1">
+      <Link to="/" className=' dark:text-white  font-bold text-4xl'>
+     <span className='px-2 py-1 bg-gradient-to-r from-indigo-600   via-purple-500 to-pink-500 rounded-lg text-white'>Usman's</span>Blog
+      </Link>
+      <p className=' text-sm pt-5'>This is a demo project and you can sign-in with your email password or with a google</p>
+      </div>
+      {/* for right side */}
+      <div className=" flex-1">
+
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
+          <div>
+            <Label value='your email'/>
+            <TextInput type='email' placeholder='yourmail@gmail.com' id='email' onChange={handleChange} />
+          </div>
+          <div>
+            <Label value='your password'/>
+            <TextInput type='password' placeholder='********' id='password' onChange={handleChange} />
+          </div>
+          <Button gradientDuoTone="purpleToPink" type='submit' disabled={loading}>
+            {
+              loading?(<>
+              <Spinner size="sm"/>
+              <span>Loadin...</span>
+              </>):
+              "SignIn"
+            }
+         
+          </Button>
+        </form>
+        <div className='text-sm mt-3 flex gap-2'>
+          <span>Dont have an account ? </span>
+          <Link to="/SignUp" className=' text-blue-400 text-sm'>sign-up</Link>
+        </div>
+      {
+        errorMessage&&(
+        <Alert className='mt-3' color="failure">
+          {errorMessage}
+        </Alert>
+        )
+      }
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default SignIn
+export default SignUp
