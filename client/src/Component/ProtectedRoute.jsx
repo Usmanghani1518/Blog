7224fec4447df86f48c5e8123d64619da0f79e18
Usman-Choudharys <mrusmanghani8@gmail.com>
2024-03-13 22:15:12 +0500

    creating the sidebar

--- client/src/Component/ProtectedRoute.jsx
+++ client/src/Component/ProtectedRoute.jsx
@@ -3,6 +3,5 @@ import { Outlet,Navigate } from 'react-router-dom'
 import {  useSelector } from 'react-redux'
 export default function ProtectedRoute() {
     const currentUser = useSelector((state)=>state.user.currentUser)
-    console.log(currentUser);
   return currentUser?<Outlet/>:<Navigate to="/SignIn"/>
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import {useLocation} from "react-router-dom"
+import DashboardSidebar from "../Component/DashSidebar.jsx";
+import DashProfile from "../Component/DashProfile.jsx";
 
 function Dashboard() {
+  const location = useLocation()
+  const[tab,setTab] = useState("");
+  useEffect(()=>{
+    const urlPrams = new URLSearchParams(location.search);
+    const tabFromUrl = urlPrams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+    
+  },[tab])
+  
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* sidebar of dashboard */}
+        <DashboardSidebar/>
+      </div>
+      {/* profile and other ... */}
+      {tab==="profile" && <DashProfile/>}
+    </div>
   )
 }
 
